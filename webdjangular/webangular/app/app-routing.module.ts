d937745efbb8871ded631a73c4d6910ff0466d93
Rafael Juzwiak <rafael_juzwiak@hotmail.com>
2018-08-23 10:52:41 -0300

    Updated angular theme, however scripts are still not being loaded

--- webdjangular/webangular/app/app-routing.module.ts
+++ webdjangular/webangular/app/app-routing.module.ts
@@ -1,62 +1,57 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import {
-    NbAuthComponent,
-    NbLoginComponent,
-    NbLogoutComponent,
-    NbRegisterComponent,
-    NbRequestPasswordComponent,
-    NbResetPasswordComponent,
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRegisterComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
 } from '@nebular/auth';
-import {PudimComponent} from "../../themes/pudim/angular/pudim.component";
 
 const routes: Routes = [
-    { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule' },
-    {
-        path: 'auth',
-        component: NbAuthComponent,
-        children: [
-            {
-                path: '',
-                component: NbLoginComponent,
-            },
-            {
-                path: 'login',
-                component: NbLoginComponent,
-            },
-            {
-                path: 'register',
-                component: NbRegisterComponent,
-            },
-            {
-                path: 'logout',
-                component: NbLogoutComponent,
-            },
-            {
-                path: 'request-password',
-                component: NbRequestPasswordComponent,
-            },
-            {
-                path: 'reset-password',
-                component: NbResetPasswordComponent,
-            },
-        ],
-    },
-    { path: 'admin', redirectTo: 'admin', pathMatch: 'full' },
-    {
-        path: '**',
-        component: PudimComponent
-
-    },
+  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
+  },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages' },
 ];
 
 const config: ExtraOptions = {
-    useHash: false,
+  useHash: false,
 };
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, config)],
-    exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes, config)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {
 }
--- webdjangular/webangular/app/app.module.ts
+++ webdjangular/webangular/app/app.module.ts
@@ -14,26 +14,23 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './@theme/theme.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { PudimModule } from "../../themes/pudim/angular/pudim.module";
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        AppRoutingModule,
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
 
-        NgbModule.forRoot(),
-        ThemeModule.forRoot(),
-        CoreModule.forRoot(),
-
-        PudimModule,
-    ],
-    bootstrap: [AppComponent],
-    providers: [
-        { provide: APP_BASE_HREF, useValue: '/' },
-    ],
+    NgbModule.forRoot(),
+    ThemeModule.forRoot(),
+    CoreModule.forRoot(),
+  ],
+  bootstrap: [AppComponent],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
 })
 export class AppModule {
 }
