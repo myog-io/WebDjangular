c99accf8c53ded2eaae7b06e5aaf8f20cf39713a
Rafael Juzwiak <rafael_juzwiak@hotmail.com>
2018-09-11 11:31:04 -0300

    removed useless imports

--- webdjangular/apps/users/views/GroupViewSet.py
+++ webdjangular/apps/users/views/GroupViewSet.py
@@ -4,17 +4,15 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
-from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.decorators import detail_route, list_route
 
 from webdjangular.apps.users.permissions.UpdateOwnUser import UpdateOwnUser
 from webdjangular.apps.users.serializers.GroupSerializer import GroupSerializer
+
+
+
 
 
 class GroupViewSet(CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
--- webdjangular/apps/users/views/PermissionViewSet.py
+++ webdjangular/apps/users/views/PermissionViewSet.py
@@ -4,15 +4,8 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import action
 from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
-from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.status import HTTP_400_BAD_REQUEST
 from rest_framework.viewsets import GenericViewSet
-
-from rest_framework_json_api.pagination import JsonApiPageNumberPagination
 
 from ..serializers.PermissionSerializer import PermissionSerializer
 
@@ -20,17 +13,10 @@ from ..serializers.PermissionSerializer import PermissionSerializer
 
 
 class PermissionViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
-    """
-    Handles:
-    Creating an User - Sign Up
-    Retrieve a list of users
-    Retrieve a specific User
-    Update an User
-    """
     resource_name = 'permission';
     serializer_class = PermissionSerializer
     queryset = Permission.objects.all()
     authentication_classes = (JSONWebTokenAuthentication,)
     filter_backends = (filters.SearchFilter,)
-    pagination_class = JsonApiPageNumberPagination
+    search_fields = ('name', )
 
