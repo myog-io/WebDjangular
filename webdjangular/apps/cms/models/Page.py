2ac2d21db6a555c614604fcf00ced8f5a7fa40c3
Rafael Juzwiak <rafael_juzwiak@hotmail.com>
2018-09-12 10:50:10 -0300

    fxied cms migration

--- webdjangular/apps/cms/models/Page.py
+++ webdjangular/apps/cms/models/Page.py
@@ -1,12 +1,12 @@
-from djongo.models import Model, CharField, TextField, SlugField, DateTimeField, ForeignKey
+from djongo import models
+from django.db import models as djangoModels
 
-
-class Page(Model):
-    title = CharField(max_length=255)
-    slug = SlugField(unique=True)
-    content = TextField()
-    created = DateTimeField(auto_now_add=True)
-    updated = DateTimeField(auto_now=True)
+class Page(models.Model):
+    title = djangoModels.CharField(max_length=255)
+    slug = djangoModels.CharField(max_length=255, null=True, default=None, blank=True)
+    content = djangoModels.TextField()
+    created = djangoModels.DateTimeField(auto_now_add=True)
+    updated = djangoModels.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
