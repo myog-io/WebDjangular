abbf1f5f09df54a69cff092f284fb3e5aaacb4c1
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2018-10-02 18:06:51 -0300

    remove webdjangular folder

--- webdjangular/apps/blog/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from webdjangular.apps.blog.models.Post import Post
-
-admin.site.register((Post,))
-
--- webdjangular/themes/pudim/angular/blog/listing/layout-b1/layout-b1.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'blog-listinglayout-b1',
-    templateUrl: './layout-b1.component.html',
-})
-export class BlogListingLayoutB1Component {
-
-}
