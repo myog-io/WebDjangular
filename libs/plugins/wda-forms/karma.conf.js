6ddc2ca8ebf938acd71fc1adb8db656e5e49dd7b
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2018-11-26 11:39:12 -0200

    plugins-contact-form -> wda-forms

--- libs/plugins/wda-forms/karma.conf.js
+++ libs/plugins/wda-forms/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
     ...baseConfig,
     coverageIstanbulReporter: {
       ...baseConfig.coverageIstanbulReporter,
-      dir: join(__dirname, '../../../coverage/libs/plugins/contact-form')
+      dir: join(__dirname, '../../../coverage/libs/plugins/wda-forms')
     }
   });
 };
---
+++ libs/plugins/wda-forms/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/wda-forms.module';
