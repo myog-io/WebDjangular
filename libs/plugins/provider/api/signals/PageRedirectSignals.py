c535ccb01398955a6e6cad21b97c8cea6323a11d
Paulo <pperes@botdoc.io>
2018-12-11 18:02:29 -0200

    Redirect Working

--- libs/plugins/provider/api/signals/PageRedirectSignals.py
+++ libs/plugins/provider/api/signals/PageRedirectSignals.py
@@ -1,3 +1,35 @@
-from django.db.models.signals import post_init, pre_init
+
 from django.dispatch import receiver, Signal
 from libs.core.cms.api.models.Page import Page
+from libs.core.cms.api.signals import pre_get_page, post_get_page
+from ..models.PageRedirect import PageRedirect
+import json
+import urllib
+@receiver(pre_get_page)
+def redirect(sender, request, *args, **kwargs):
+
+    if request.COOKIES.get('clientUser'):
+        cookie_val = request.COOKIES.get('clientUser')
+        cookie_val = urllib.parse.unquote(cookie_val)
+        client = json.loads(cookie_val)
+
+        if 'data' in client:
+            data = client['data']
+
+            if 'city' in data:
+                city = data['city']
+                if 'id' in city:
+                    filter_args = {}
+                    if 'slug' in kwargs:
+                        filter_args['default_page__slug'] = kwargs['slug']
+                    else:
+                        filter_args['default_page__pk'] = kwargs['pk']
+
+                    filter_args['cities__pk'] = city['id']
+
+                    pageRedirect = PageRedirect.objects.filter(**filter_args).first()
+                    if pageRedirect:
+                        kwargs['slug'] = pageRedirect.redirect_page.slug
+                        kwargs['pk'] = pageRedirect.redirect_page.pk
+                        print("FOUND PAGE REDIRECT,",kwargs)
+    return kwargs
--- libs/plugins/provider/src/lib/components/errors/500/500.component.ts
+++ libs/plugins/provider/src/lib/components/errors/500/500.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'wda-error-500',
   styleUrls: ['./500.component.scss'],
   templateUrl: './500.component.html',
+  encapsulation: ViewEncapsulation.None
 })
 export class  PluginProviderError500Component{
 
