0a8bf3529c60aea14abd713ba329bc4f1899ffb1
Paulo <pperes@botdoc.io>
2018-12-28 12:13:57 -0200

    Making City List

--- libs/plugins/provider/src/lib/components/plans/pricing/combo-vertical/combo-vertical.component.ts
+++ libs/plugins/provider/src/lib/components/plans/pricing/combo-vertical/combo-vertical.component.ts
@@ -36,13 +36,13 @@ export class PluginPricingComboVerticalComponent implements OnInit {
       this.tv = entries.find( (product) => product.sku == this.sku_tv );
       this.phone = entries.find( (product) => product.sku == this.sku_phone );
       if(this.net && this.net.price){
-        this.total += parseFloat(this.net.price);
+        this.total += this.net.price;
       }
       if(this.tv && this.tv.price){
-        this.total += parseFloat(this.tv.price);
+        this.total += this.tv.price;
       }
       if(this.phone && this.phone.price){
-        this.total += parseFloat(this.phone.price);
+        this.total += this.phone.price;
       }
       // TODO: Apply Discount???
 
