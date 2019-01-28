3135f53350692abe60e251df36d52fafb5863335
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2019-01-28 13:42:32 -0200

    Provider Checkout: Token cart shareable

--- libs/plugins/provider/src/lib/components/checkout/wizard/step02/step02.component.ts
+++ libs/plugins/provider/src/lib/components/checkout/wizard/step02/step02.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import {ProviderCheckoutService} from "../../../../data/services/provider-checkout.service";
+import {NgbTooltipConfig} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'plugin-provider-checkout-wizard-step02',
   templateUrl: './step02.component.html',
-  styleUrls: ['./step02.component.scss']
+  styleUrls: ['./step02.component.scss'],
 })
 export class PluginProviderCheckoutWizardStep02Component implements OnInit {
 
-  constructor(public providerCheckout: ProviderCheckoutService) { }
+  constructor(public providerCheckout: ProviderCheckoutService) {
+  }
 
   ngOnInit() {
+
   }
 
 }
