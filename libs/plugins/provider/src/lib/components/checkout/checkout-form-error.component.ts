df7ddc439a9dd7bd9ca3a93745b83c2a717479ed
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2019-01-29 21:43:31 -0200

    Provider Checkout Updates

--- libs/plugins/provider/src/lib/components/checkout/checkout-form-error.component.ts
+++ libs/plugins/provider/src/lib/components/checkout/checkout-form-error.component.ts
@@ -1,25 +1,27 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {ProviderCheckoutService, ProviderCheckoutSteps} from "../../data/services/provider-checkout.service";
-import {ActivatedRoute} from "@angular/router";
-import { FormControl } from '@angular/forms';
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'plugin-provider-checkout-error',
   template: `
     <div class="errors-feedback" *ngIf="ctrl.touched && ctrl.errors">
-        
-
-        <div *ngIf="ctrl.errors.required" class="alert alert-danger" role="alert">   {{name}} é obrigatório </div>
-        <div *ngIf="ctrl.errors.email || ctrl.errors.minlength || ctrl.errors.maxlength" class="alert alert-danger"role="alert"> {{name}} inválido </div>
+      <div *ngIf="ctrl.errors.required" class="alert alert-danger" role="alert">
+        <span *ngIf="!message">{{name}} é obrigatório </span>
+        <span *ngIf="message">{{message}}</span>
+      </div>
+      <div *ngIf="ctrl.errors.email || ctrl.errors.minlength || ctrl.errors.maxlength" class="alert alert-danger"
+           role="alert"> {{name}} inválido
+      </div>
     </div>
-`,
+  `,
 })
 export class PluginProviderCheckoutErrorComponent implements OnInit {
-    @Input() ctrl:FormControl;
-    @Input() name:string = "Campo"
+  @Input() ctrl: FormControl;
+  @Input() name: string = "Campo";
+  @Input() message: string = "";
 
   ngOnInit() {
-    
+
   }
 
 }
