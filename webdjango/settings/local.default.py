5f3b38827066418dce782506373e17cb91be9a98
Paulo Peres <pperes@botdoc.io>
2019-05-13 22:12:10 -0300

    Default Redis Config

--- webdjango/settings/local.default.py
+++ webdjango/settings/local.default.py
@@ -31,3 +31,13 @@ DATABASES = {
         'NAME': 'your-db-name',
     }
 }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            #"PASSWORD": ""
+        }
+    }
+}
