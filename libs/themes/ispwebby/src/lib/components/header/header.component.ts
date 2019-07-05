b641aed0b3ad1eda191d512cb29e67e1bb1d0ce5
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2019-07-05 13:52:15 -0300

    remove the IspWebby theme

--- libs/themes/ispwebby/src/lib/components/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ThemeIspwebbyComponent } from '../../ispwebby.component';
-
-@Component({
-  selector: 'theme-ispwebby-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html'
-})
-export class ThemeIspwebbyHeaderComponent {}
