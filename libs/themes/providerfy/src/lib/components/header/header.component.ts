5ca55dfd8b855a267cf5f97cc80eeefe10aade4e
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2019-07-10 13:18:38 -0300

    clean Libs/Themes for SubModules

--- libs/themes/providerfy/src/lib/components/header/header.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { ThemeProviderfyComponent } from '../../providerfy.component';
-
-@Component({
-  selector: 'theme-providerfy-header',
-  styleUrls: ['./header.component.scss'],
-  templateUrl: './header.component.html'
-})
-export class ThemeProviderfyHeaderComponent {}
